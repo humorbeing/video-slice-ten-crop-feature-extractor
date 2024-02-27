22f156b9689d1618f8354ca012e546059e72fef2
ray <geemguang@gmail.com>
2024-02-27 17:17:09 +0000

    on home-pc with pytorch 221

--- old_files_here/one_video_feature_extract_v0001.py
+++ old_files_here/one_video_feature_extract_v0001.py
@@ -8,10 +8,9 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model.eval()
 model.to(device)    
 
-from torchvision.io.video import read_video
-# pip install av
-video_path = './videos/fast.mp4'
-video_path = './videos/slow.mp4'
-rgb, audio, info = read_video(video_path, pts_unit='sec')
+input_features = torch.randn(1,3,16,224,224)
+input_features = input_features.to(device)
+
+outputs = model(input_features, features=True)
 
 print('end')
